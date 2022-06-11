1d233d1d657d78f0924c3278433e9ba0e28cd82f
Aleks Debrov <aleksei-debrov@mail.ru>
2022-06-11 18:06:55 +0300

    Added console.log

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,21 @@
-// alert(123);
+let money = 105;
+let income = 'Videograph';
+let addExpenses = 'internet, taxi, communal';
+let deposit = true;
+let mission = 1000000;
+let period = 4;
 
-// console.log(2);
+console.log(typeof money);
+console.log(typeof income);
+console.log(addExpenses .length);
+console.log(typeof deposit);
+console.log(typeof mission);
+console.log(typeof period);
 
+console.log(`Период равен ${period} месяцев`);
 
+console.log(addExpenses .toUpperCase());
+console.log(addExpenses.split(','))
 
-//однострочный комментарий
-
-/* многострочный
-комментарий */
-
-
-
-var myVar = 10;
+let budgetDay = 1500
+console.log(budgetDay)
